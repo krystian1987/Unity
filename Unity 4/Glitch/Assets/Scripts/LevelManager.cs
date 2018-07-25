8298c8b6c6ff57e9cc11c17e732bf768b8faa0f8
Unknown <krystian.dabrowski.1987@gmail.com>
2018-07-25 23:15:27 +0200

    Added Glitch project

---
+++ Unity 4/Glitch/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+  public float autoLoadNextLevel;
+
+  void Start()
+  {
+    if (autoLoadNextLevel != 0)
+    {
+      Invoke("LoadNextLevel", autoLoadNextLevel);
+    }
+  }
+
+
+  public void LoadLevel(string name)
+  {
+    Application.LoadLevel(name);
+    Debug.Log("Level load requested for: " + name);
+  }
+
+  public void ExitLevel(string name)
+  {
+    Application.Quit();
+    Debug.Log("Level exit requested for: " + name);
+  }
+
+  public void LoadNextLevel()
+  {
+    Application.LoadLevel(Application.loadedLevel + 1);
+  }
+
+  public void BrickDestoyed()
+  {
+    if (Brick.breakableCount <= 0)
+    {
+      LoadNextLevel();
+    }
+  }
+
+}
