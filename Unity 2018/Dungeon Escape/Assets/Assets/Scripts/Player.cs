891d2edfbada10a4c9c818c87f156bb9126a1dd6
Krystian <krystian.dabrowski.1987@gmail.com>
2018-11-14 22:45:29 +0100

    Added animations of run and jump

--- Unity 2018/Dungeon Escape/Assets/Assets/Scripts/Player.cs
+++ Unity 2018/Dungeon Escape/Assets/Assets/Scripts/Player.cs
@@ -1,64 +1,72 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
   private Rigidbody2D _rigidbody2D;
-  private bool _isGround = false;
   private bool _resetJump = false;
   [SerializeField]
-  private float _jumpForce = 5f;
+  private float _jumpForce = 5.0f;
+  [SerializeField]
+  private float _playerSpeed = 10.0f;
+
+  private Animator _animator;
+  private PlayerAnimation _playerAnimation;
+  private SpriteRenderer _playerSpriteRenderer;
 
   // Use this for initialization
   void Start ()
   {
     _rigidbody2D = GetComponent<Rigidbody2D>();
-
+    _playerAnimation = GetComponent<PlayerAnimation>();
+    _playerSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
   }
 	
 	// Update is called once per frame
   void Update()
   {
-    var move = Input.GetAxisRaw("Horizontal");
+    Movement();
+  }
 
-    if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+  private void Movement()
+  {
+    var move = Input.GetAxisRaw("Horizontal");
+    IsGrounded();
+
+    if (move != 0)
+      _playerSpriteRenderer.flipX = (move < 0);
+
+    if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
     {
       _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
-      _isGround = false;
-      _resetJump = true;
+      _playerAnimation.Jump(true);
       StartCoroutine(ResetJumpNeedRoutine());
     }
 
-    RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
-    Debug.DrawRay(transform.position,Vector3.down * 0.6f, Color.green);
+    _rigidbody2D.velocity = new Vector2(move * _playerSpeed, _rigidbody2D.velocity.y);
+
+    _playerAnimation.Move(move);
+
+  }
+
+  private bool IsGrounded()
+  {
+    RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
     if (hitInfo.collider != null)
     {
-      Debug.Log("hit " + hitInfo.collider.name);
       if (_resetJump == false)
       {
-        _isGround = true;
-        
+        _playerAnimation.Jump(false);
+        return true;
       }
     }
-
-    _rigidbody2D.velocity = new Vector2(move, _rigidbody2D.velocity.y);
-    _resetJump = false;
+    return false;
   }
 
   private IEnumerator ResetJumpNeedRoutine()
   {
+    _resetJump = true;
     yield return new WaitForSeconds(0.1f);
     _resetJump = false;
   }
-
-  //void OnCollisionEnter2D(Collision2D col)
-  //{
-  //  _isGround = true;
-  //}
-
-  //void OnCollisionExit2D(Collision2D col)
-  //{
-  //  _isGround = false;
-  //}
 }
---
+++ Unity 2018/Dungeon Escape/Assets/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+  private Animator _animator;
+
+  // Use this for initialization
+	void Start () {
+	  _animator = GetComponentInChildren<Animator>();
+  }
+
+  public void Move(float move)
+  {
+    _animator.SetFloat("Move", Math.Abs(move));
+  }
+
+  public void Jump(bool jumping)
+  {
+    _animator.SetBool("Jump", jumping);
+  }
+}
