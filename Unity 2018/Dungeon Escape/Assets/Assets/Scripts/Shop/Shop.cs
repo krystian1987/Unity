d59c3b9a3004896d3a2f3059fb4874490be354ca
Krystian <krystian.dabrowski.1987@gmail.com>
2018-12-29 01:01:18 +0100

    Start with Shop UI

---
+++ Unity 2018/Dungeon Escape/Assets/Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+
+{
+  public GameObject ShopPanel;
+  private 
+  void OnTriggerEnter2D(Collider2D collider2D)
+  {
+    if (collider2D.tag == "Player")
+    {
+      Player player = collider2D.GetComponent<Player>();
+      if (player != null)
+      {
+       UIManager.Instance.OpenShop(player.diamonds);
+      }
+      ShopPanel.SetActive(true);
+    }
+  }
+
+  void OnTriggerExit2D(Collider2D collider2D)
+  {
+    if (collider2D.tag == "Player")
+    {
+      ShopPanel.SetActive(false);
+    }
+  }
+}
---
+++ Unity 2018/Dungeon Escape/Assets/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Persistence;
+
+public class UIManager : MonoBehaviour
+{
+
+  private static UIManager _instance;
+
+  public static UIManager Instance
+  {
+    get
+    {
+      if (_instance == null)
+      {
+        throw new UnityException("UIManager instance is null.");
+      }
+      return _instance;
+    }
+  }
+
+  public Text PlayerGemCountText;
+
+  public void OpenShop(int gemCount)
+  {
+    PlayerGemCountText.text = $"{gemCount.ToString()}G";
+  }
+
+  private void Awake()
+  {
+    _instance = this;
+  }
+}
